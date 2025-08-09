323450c6be1fa53d8c93641cecf3b63bb6885893
Sivaani J <65666765+Sivaani-Janaswamy@users.noreply.github.com>
2025-08-09 12:24:39 +0530

    Time: 21 ms (19.82%) | Memory: 59.8 MB (20.20%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i:nums){
+            set.add(i);
+        }
+        if(set.size()==nums.length){
+            return false;
+        }
+        return true;
+    }
+}
