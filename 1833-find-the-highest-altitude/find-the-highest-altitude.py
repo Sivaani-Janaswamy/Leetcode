0b4c2482603ff5868a57318e6f2c11db8d9aa69c
Sivaani J <65666765+Sivaani-Janaswamy@users.noreply.github.com>
2026-03-21 13:44:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (77.34%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.py
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.py
@@ -5,7 +5,8 @@ class Solution(object):
         :rtype: int
         """
         n = len(gain)
-        prefix = [0]*(n+1)
+        sums,maxi = 0,0
         for i in range(1,n+1):
-            prefix[i] = prefix[i-1] + gain[i-1] 
-        return max(prefix)       
+            sums = sums + gain[i-1] 
+            maxi = max(sums,maxi)
+        return maxi       
